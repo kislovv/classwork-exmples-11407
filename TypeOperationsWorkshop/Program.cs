f7734cdc7d16ef3a7963f4b5e5a9d3fc7f23742d
Kislov Kirill <kislov-k@inbox.ru>
2024-11-07 09:56:03 +0300

    add form

--- TypeOperationsWorkshop/Program.cs
+++ TypeOperationsWorkshop/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+		string name = Console.ReadLine();
+		int age = int.Parse(Console.ReadLine());
+		int course = 2024-2015;
+		string groupNumber = Console.ReadLine();
+		bool hasHomePet = age > 50;
+		
+        Console.WriteLine("Анкета студента:");
+        Console.WriteLine($"Вас зовут {name}");
+        Console.WriteLine("Вам " + age + " лет");
+        Console.WriteLine("Вы на " + course + " курсе");
+        Console.WriteLine("В группе " + groupNumber);
+        Console.WriteLine("Есть ли у вас домашний питомец: " + hasHomePet);		
+	
     }
 }
